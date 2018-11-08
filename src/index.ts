62a11b72bfdd6ecce489bdc9e786170e087a3e13
YinYin Chiu <cychiuae@connect.ust.hk>
2018-11-08 23:29:27 +0800

    Create simple program

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
-function sayHello(name: string) {
-  console.log(`Hello ${name}`);
-}
+import * as program from "commander";
 
-sayHello("World");
+program.version("1.0.0").description("Scaffolding for skygear server");
+
+program.on("command:*", () => {
+  console.error("Invalid command: %s", program.args.join(" "));
+  program.help();
+});
+
+program.parse(process.argv);
+if (process.argv.slice(2).length === 0) {
+  console.error(
+    "You didn't pass any command\nSee --help for a list of available commands."
+  );
+}
