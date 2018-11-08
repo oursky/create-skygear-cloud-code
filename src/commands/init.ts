c3bda2ecfd3b742570e7eeeb65721432188dab80
YinYin Chiu <cychiuae@connect.ust.hk>
2018-11-08 23:44:07 +0800

    Validate project name

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,7 +1,20 @@
 import { CommanderStatic } from "commander";
+import { validateProjectName } from "src/validation";
 
 export function registerCommand(program: CommanderStatic) {
-  program.command("init").action(() => {
-    console.log("Init a skygear server");
-  });
+  program
+    .command("init <projectName>")
+    .description("Create a skygear server")
+    .action((projectName: string) => {
+      const isProjectNameValid = validateProjectName(projectName);
+      if (!isProjectNameValid) {
+        console.error(
+          `${projectName} is not a valid name for a project.` +
+            "Please use a valid identifier name (alphanumeric)."
+        );
+        return;
+      }
+
+      console.log("Init a skygear server");
+    });
 }
