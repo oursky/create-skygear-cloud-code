18df3531ddc05f5ab3500b63a43d769f25322b91
YinYin Chiu <cychiuae@connect.ust.hk>
2018-11-08 23:31:28 +0800

    Add help command

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import * as program from "commander";
 
+import { registerCommand } from "./commands/help";
+
 program.version("1.0.0").description("Scaffolding for skygear server");
 
 program.on("command:*", () => {
   console.error("Invalid command: %s", program.args.join(" "));
   program.help();
 });
+
+registerCommand(program);
 
 program.parse(process.argv);
 if (process.argv.slice(2).length === 0) {
